2f9a4ecab65c3fd64a6789df685215327f2933d5
gabrielSantos1914 <gabrielfelipedossantos1914@gmail.com>
2023-07-07 10:00:31 -0300

    Pagina inicial 0k

---
+++ js/carrossel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carrossel').slick({
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    speed: 1500,
+    slideToShow: 5,
+    autoplayspeed: 3000
+});
+})
